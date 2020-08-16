8e15146fccf40da28ce5d12d221e130c44894687
hiteshsreevamsi <hiteshsreevamsis@gmail.com>
2020-08-16 20:16:39 +0530

    Adding test cases

--- src/Grid/__test__/Adduser.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GridLayout from "../GridLayout";
-
-test("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<GridLayout></GridLayout>, div);
-});
